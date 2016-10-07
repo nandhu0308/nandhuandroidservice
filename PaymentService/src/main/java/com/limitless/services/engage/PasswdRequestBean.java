9585750bcb0ed10f07cd394ef33e44a48d4ee3ad
ramk_1011 <ram@limitlesscircle.com>
2016-10-07 23:45:03 +0530

    Commit 07/10/2016 [2]
    1. Added password changing functionality
    2. New Classes
          PasswdRequestBean.java
          PasswdResponseBean.java

---
+++ PaymentService/src/main/java/com/limitless/services/engage/PasswdRequestBean.java
@@ -0,0 +1,21 @@
+package com.limitless.services.engage;
+
+public class PasswdRequestBean {
+	
+	private String oldPasswd;
+	private String newPasswd;
+	
+	public String getOldPasswd() {
+		return oldPasswd;
+	}
+	public void setOldPasswd(String oldPasswd) {
+		this.oldPasswd = oldPasswd;
+	}
+	public String getNewPasswd() {
+		return newPasswd;
+	}
+	public void setNewPasswd(String newPasswd) {
+		this.newPasswd = newPasswd;
+	}
+
+}
