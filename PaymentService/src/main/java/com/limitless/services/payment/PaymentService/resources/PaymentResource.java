2624c5775859d449a245b12b6262ff419401bbd1
ramk_1011 <ram@limitlesscircle.com>
2016-10-12 19:46:29 +0530

    Commit 12/10/2016 Auth Token Updated.

--- PaymentService/src/main/java/com/limitless/services/payment/PaymentService/resources/PaymentResource.java
+++ PaymentService/src/main/java/com/limitless/services/payment/PaymentService/resources/PaymentResource.java
@@ -234,7 +234,7 @@ public class PaymentResource {
 
 				ClientResponse splitResponse = webResource.accept("application/json").
 						type("application/json")
-						.header("auth_token","eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhY2Nlc3Nfa2V5IjoiOTZLMzRRNEE4SThOUkVIN05WT1oiLCJleHBpcmVzIjoiMjAxNi0xMC0xMVQxMTo0MTo1MS44NTRaIiwiY2FuX3RyYW5zYWN0IjoxLCJhZG1pbiI6MH0.c3JEDaOW6is2xAk2uwuiHSt3LGza4Iq2XMjJEpvA8Bg")
+						.header("auth_token","eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhY2Nlc3Nfa2V5IjoiOTZLMzRRNEE4SThOUkVIN05WT1oiLCJleHBpcmVzIjoiMjAxNi0xMC0xN1QxMzoxOToyMC4wNDVaIiwiY2FuX3RyYW5zYWN0IjoxLCJhZG1pbiI6MH0.e0tIMQtjMv2SLvz-mQVW2ktjM0cjLyrA_Vtmv6SJfQU")
 						.post(ClientResponse.class, splitRequest);
 				
 				String splitResponseStr = splitResponse.getEntity(Object.class).toString();
