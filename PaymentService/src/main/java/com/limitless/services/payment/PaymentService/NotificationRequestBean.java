8c837815d8c339fb6b06c4485c6ee5e7b72a0aa0
ramk_1011 <ram@limitlesscircle.com>
2016-11-17 15:42:23 +0530

    Commit 17/11/2016 priority added for FCM request

--- PaymentService/src/main/java/com/limitless/services/payment/PaymentService/NotificationRequestBean.java
+++ PaymentService/src/main/java/com/limitless/services/payment/PaymentService/NotificationRequestBean.java
@@ -2,6 +2,7 @@ package com.limitless.services.payment.PaymentService;
 
 public class NotificationRequestBean {
 	private String to;
+	private String priority = "high";
 	private NotificationBean notification;
 
 	public String getTo() {
@@ -18,6 +19,14 @@ public class NotificationRequestBean {
 
 	public void setNotification(NotificationBean notification) {
 		this.notification = notification;
+	}
+
+	public String getPriority() {
+		return priority;
+	}
+
+	public void setPriority(String priority) {
+		this.priority = priority;
 	}
 
 }
