1d8a7bfc04a4cac39d97a92b1e5c94d58b1136f4
Anandh <anandh@limitlesscircle.com>
2017-02-23 13:02:52 +0530

    commit

---
+++ PaymentService/src/main/java/com/limitless/services/engage/SellerRequestBean.java
@@ -0,0 +1,20 @@
+package com.limitless.services.engage;
+
+public class SellerRequestBean {
+private int customerId;
+private String searchString;
+
+public int getCustomerId() {
+	return customerId;
+}
+public void setCustomerid(int customerid) {
+	this.customerId = customerid;
+}
+public String getSearchString() {
+	return searchString;
+}
+public void setSearchString(String searchString) {
+	this.searchString = searchString;
+}
+
+}
